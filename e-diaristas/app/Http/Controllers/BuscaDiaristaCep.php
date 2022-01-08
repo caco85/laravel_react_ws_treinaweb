54859a87667f39e56daed234dbdaee5d82c5f473
caco85 <renato_caco85@hotmail.com>
2022-01-08 15:04:51 -0300

    workshop treina web multi stack , react+laravel+react native

---
+++ e-diaristas/app/Http/Controllers/BuscaDiaristaCep.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BuscaDiaristaCep extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $endereco = $viaCEP->buscar($request->cep);
+
+        if ($endereco === false) {
+            return response()->json(['erro'=>'Cep inválido'], 400);
+        }
+
+        return [
+            'diaristas' => Diarista::buscaPorCodigoIbge($endereco['ibge']),
+            'quantidade_diaristas' => Diarista::quantidadePorCodigoIbge($endereco['ibge'])
+        ];
+    }
+}
